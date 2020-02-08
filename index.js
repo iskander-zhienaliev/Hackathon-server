8572afc81a60446f4717fe69f91ba7ca93d017b1
Igor <46229356+NekroMagus@users.noreply.github.com>
2020-02-09 02:21:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from './src/server';
 
-const SERVER_PORT = process.env.SERVER_PORT || 8080;
+const SERVER_PORT = process.env.SERVER_PORT || 9990;
 
 app.listen(SERVER_PORT, () => {
     console.log('Server started');
